4370cef1fb343016bbaac1ada379f3de01f4cea4
Ahmad Fajruddin Syauqi <fajrudinsauqi@gmail.com>
2022-07-27 10:34:12 +0700

    Report

---
+++ src/mentalku/Auth.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mentalku;
+
+/**
+ *
+ * @author AF Syauqi
+ */
+public interface Auth {
+    public void login();
+    public void register();
+}
--- src/mentalku/Mentalku.java
+++ src/mentalku/Mentalku.java
@@ -19,51 +19,9 @@ public class Mentalku {
      * @param args the command line arguments
      */
    public static void main(String[] args) {
-                // InputData id = new InputData();
-                // id.setVisible(true);
+       
                 Login login = new Login();
                 login.setVisible(true);
-                
-                // Scanner masukan = new Scanner(System.in);
-                // CekOrang anan = new CekOrang();
-                // System.out.print("Masukkan Nama Anda: ");
-                // anan.setNama(masukan.nextLine());
-                // // System.out.print("Masukkan Umur Anda: ");
-                // // anan.setUmur(masukan.nextInt());
-                // System.out.print("Suhu Badan Anda dalam Derajat Farenheit (82-102 *F): ");
-                // anan.setSuhu(masukan.nextDouble());
-                // System.out.print("Rasio Limb Movement Anda (0-20): ");
-                // anan.setLimb(masukan.nextDouble());
-                // System.out.print("Kadar Oksigen dalam Darah Anda (%): ");
-                // anan.setOksigen(masukan.nextDouble());
-                // System.out.print("Berapa Detak Jantung Anda per Menit? (40-120): ");
-                // anan.setDetak(masukan.nextDouble());
-                // masukan.close();
-                // System.out.println("\n");
-                
-                // anan.setFrekuensiPrediksi();
-                // System.out.println("Nama Anda: " + anan.getNama());
-                // // System.out.println("Umur Anda: " + anan.getUmur());
-                // System.out.println("Suhu Badan Anda dalam Derajat Farenheit (*F): " + anan.getSuhu());
-                // System.out.println("Rasio Limb Movement per Periodik: " + anan.getLimb());
-                // System.out.println("Kadar Oksigen dalam Darah (%): " + anan.getOksigen());
-                // System.out.println("Detak Jantung per Menit: " + anan.getDetak());
-                // System.out.println("\n");
-
-                // anan.setJenisSuhu();
-                // anan.setJenisLimb();
-                // anan.setJenisOksigen();
-                // anan.setJenisDetak();
-                // anan.setFYaTidak(anan.getJenisLimb(), anan.getJenisSuhu(), anan.getJenisOksigen(),
-                //                 anan.getJenisDetak());
-
-                // anan.ProbYa(anan.getFYa(), anan.getFTidak());
-                // anan.ProbTidak(anan.getFYa(), anan.getFTidak());
-                // anan.ProbNBayesYa(anan.getProbYa(), anan.getProbTidak());
-                // anan.ProbNBayesTidak(anan.getProbYa(), anan.getProbTidak());
-                // System.out.println("Prediksi Kondisi Kecemasan Anda Beserta Persentase: " + "\"" + anan.prediksi() + "\"");
-                // System.out.println("Probabilitas Cemas: " + anan.getProbNBayesYa());
-                // System.out.println("Probabilitas Tidak Cemas: " + anan.getProbNBayesTidak());
         }
     
 }
